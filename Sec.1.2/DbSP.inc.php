e3adccbd81e83de141438b78a4f8521a31d86d8a
Peter Myrvig <myrvig@gmail.com>
2018-04-16 11:04:25 +0200

    op

---
+++ Sec.1.2/DbSP.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * DbSP.inc.php
+ * parameters for secure sessions and database connectivity
+ * @author nml
+ * @copyright (c) 2018, nml
+ * @license http://www.fsf.org/licensing/ GPLv3
+ */
+abstract class DbSP {
+    // database connectivity
+    const DBHOST = 'localhost';     // host
+    const DBUSER = 'nobody';        // mysql user
+    const USERPWD = 'test';         // mysql password
+    const DB = 'yaddaSecVariant';   // current database
+    const DSN = "mysql:host=".self::DBHOST.";dbname=".self::DB;
+
+    // secure sessions
+    const SECURE = false;           // dev only, true in production
+    const HTTP_ONLY = true;         // enforces cookie only sessions
+    const SESS_NAME = 'nml42';       // application session name
+}
