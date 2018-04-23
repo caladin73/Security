97db3657d210236f68d77f51d60faf3f7b0d21e2
Peter Myrvig <myrvig@gmail.com>
2018-04-23 11:13:16 +0200

    op

--- Sec.3.0/login0Auth.php
+++ Sec.3.0/login0Auth.php
@@ -6,24 +6,25 @@ $dbh = DbH::getDbH();
 // if there is content in POST authenticate
 // sqlinj vulnerable
 if (count($_POST) > 0) {
-$sql = "select uid, realname, pwd";
-$sql .= " from user";
-$sql .= " where uid = '". $_POST['user'] ."'";
-$sql .= " and activated;";
-try {
-$s = $dbh->prepare($sql);
-$s->execute();
-$obj  = $s->fetch(PDO::FETCH_OBJ);
-if ($obj && password_verify($_POST['password'], $obj->pwd)) {
-$_SESSION['demoLoginId'] = $obj->uid;
-header("Location: ./login0.php?success");
+    $sql = "select uid, realname, pwd";
+    $sql .= " from user";
+    $sql .= " where uid = '". $_POST['user'] ."'";
+    $sql .= " and pwd = '" . password_hash($_POST['password'], PASSWORD_DEFAULT) . "'";
+    $sql .= " and activated;";
+    try {
+        $s = $dbh->prepare($sql);
+        $s->execute();
+        $obj  = $s->fetch(PDO::FETCH_OBJ);
+        if ($obj) {
+            $_SESSION['demoLoginId'] = $obj->uid;
+            header("Location: ./login0.php?success");
+        } else {
+            unset($_SESSION['demoLoginId']);
+            header("Location: ./login0.php?err=noSuccess");
+        }
+    } catch (PDOException $e) {
+        die(sprintf("Unexpected error<br/>\n", $e->getMessage()));
+    }
 } else {
-unset($_SESSION['demoLoginId']);
-header("Location: ./login0.php?err=noSuccess");
-}
-} catch (PDOException $e) {
-die(sprintf("Unexpected error<br/>\n", $e->getMessage()));
-}
-} else {
-header("Location: ./login0.php?err=noData");
+    header("Location: ./login0.php?err=noData");
 }
