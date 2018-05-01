077058ce5350b7bcc4e44bdada69d8ff92b0c109
Peter Myrvig <myrvig@gmail.com>
2018-05-01 10:56:51 +0200

    op

--- Sec.5.0/login0p1.php
+++ Sec.5.0/login0p1.php
@@ -7,22 +7,30 @@ if (!
     exit();
 }
 
-
+//INSERT FORM
 
 
 
 ?><!doctype html>
 <html>
 <head>
-    <title>Login Demo  Page 1</title>
+    <title>Insert form page</title>
     <meta charset='utf-8'/>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
 </head>
 <body>
-<h1>Page 1 - You're In</h1>
+<h1>You are logged in, insert data, or..</h1>
 <p>
     Go back to
     <a href='./login0.php'>Forside</a>
 </p>
+
+<form>
+    First name:<br>
+    <input type="text" name="firstname"><br>
+    Last name:<br>
+    <input type="text" name="lastname">
+</form>
+
 </body>
 </html>
